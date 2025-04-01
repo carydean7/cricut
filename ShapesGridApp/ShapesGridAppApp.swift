dd34f2343568ebc9cd22a2ce8bf51bde62fcc5b6
Dean Wagstaff <dean.wagstaff@amerigas.com>
2025-03-31 20:14:57 -0400

    Initial Commit

---
+++ ShapesGridApp/ShapesGridAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesGridAppApp.swift
+//  ShapesGridApp
+//
+//  Created by Dean Wagstaff on 3/31/25.
+//
+
+import SwiftUI
+
+@main
+struct ShapesGridAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShapesGridAppTests/ShapesGridAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesGridAppTests.swift
+//  ShapesGridAppTests
+//
+//  Created by Dean Wagstaff on 3/31/25.
+//
+
+import Testing
+@testable import ShapesGridApp
+
+struct ShapesGridAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
